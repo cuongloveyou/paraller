39ea04a653fd3a1feb315c2689354c641f82365f
cuongloveyou <55919737+cuongloveyou@users.noreply.github.com>
2020-06-24 22:21:00 +0700

    Add files via upload

--- done_final/memory_11.v
+++ done_final/memory_11.v
@@ -6,35 +6,32 @@ input [7:0]pixelw,
 output reg [7:0]pixelr1,pixelr2,pixelr3,pixelr4,pixelr5,pixelr6,pixelr7,pixelr8,pixelr9
 );
 
-parameter _1b1 = 1'b1;
-
-reg [7:0]mem_read[0:8771];
-reg [7:0]mem_write[0:8771];
-reg [14:0]i;
-reg [8:0]j;
-reg [14:0]cnt;
+reg [7:0]mem_read[0:65][0:65];
+reg [7:0]mem_write[0:65][0:65];
+reg [6:0]i, ii;
+reg [6:0]j, jj;
 
 always @(posedge clk)
 begin
 if (!rst_n)
   begin
-    i <= 0;
-    j <= 0;
+    i <= 0; ii <= 0;
+    j <= 0; jj <= 0;
   end
 else if(rd == 1)
 	begin
-	pixelr1 <= mem_read[i+j];
-	pixelr2 <= mem_read[i+j+1];
-	pixelr3 <= mem_read[i+j+2];
-	pixelr4 <= mem_read[i+j+258];
-	pixelr5 <= mem_read[i+j+258+1];
-	pixelr6 <= mem_read[i+j+258+2];
-	pixelr7 <= mem_read[i+j+516];
-	pixelr8 <= mem_read[i+j+516+1];
-	pixelr9 <= mem_read[i+j+516+2];
+	pixelr1 <= mem_read[i][j];
+	pixelr2 <= mem_read[i][j+1];
+	pixelr3 <= mem_read[i][j+2];
+	pixelr4 <= mem_read[i+1][j];
+	pixelr5 <= mem_read[i+1][j+1];
+	pixelr6 <= mem_read[i+1][j+2];
+	pixelr7 <= mem_read[i+2][j];
+	pixelr8 <= mem_read[i+2][j+1];
+	pixelr9 <= mem_read[i+2][j+2];
 	
-	j <= (j == 255) ? 0:(j+1);
-	i <= (j == 255) ? (i+258):i;
+	j <= (j == 63) ? 0:(j+1);
+	i <= (j == 63) ? (i+1):i;
 	end
 else
 	begin
@@ -48,24 +45,15 @@ else
 	pixelr8 <= 8'b0;
 	pixelr9 <= 8'b0;
 	end
-end
-always @(posedge clk)
-begin
-if(!rst_n)
-begin
-    cnt <= 0;
-end
-else
-if(wr)
+	if(wr)
 	begin
-	mem_write[cnt] <= pixelw;
-  	cnt <= cnt + _1b1;
+	 mem_write[ii][jj] <= pixelw;
+  	
+  	ii <= i - 3;
+  	jj <= j - 3;
 	end
-else
-   mem_write[cnt] <= 0;
-  
+  else
+   mem_write[ii][jj] <= 0;  
 end
-
 endmodule
-
 
--- done_final/parallerFilter_tb.v
+++ done_final/parallerFilter_tb.v
@@ -1,8 +1,8 @@
 module parallerFilter_tb();
-  integer i;
+  integer i,j;
   reg clk,rst_n,en;
-  reg [7:0] mem_read1[0:4643], mem_read2[0:4643], mem_read3[0:4643], mem_read4[0:4643], mem_read5[0:4643], mem_read6[0:4643], mem_read7[0:4643], mem_read8[0:4643],
-            mem_read9[0:4643], mem_read10[0:4643], mem_read11[0:4643], mem_read12[0:4643], mem_read13[0:4643], mem_read14[0:4643], mem_read15[0:4643], mem_read16[0:4643];
+  reg [7:0] mem_read1[0:65][0:65], mem_read2[0:65][0:65], mem_read3[0:65][0:65], mem_read4[0:65][0:65], mem_read5[0:65][0:65], mem_read6[0:65][0:65], mem_read7[0:65][0:65], mem_read8[0:65][0:65],
+            mem_read9[0:65][0:65], mem_read10[0:65][0:65], mem_read11[0:65][0:65], mem_read12[0:65][0:65], mem_read13[0:65][0:65], mem_read14[0:65][0:65], mem_read15[0:65][0:65], mem_read16[0:65][0:65];
   wire rd;
   wire [7:0]cl_pixel;
   parallerFilter pp( clk, rst_n, en, rd, cl_pixel);
@@ -10,68 +10,84 @@ module parallerFilter_tb();
 initial 
 begin
 $readmemh("memory_1.txt",mem_read1);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem1.mem_read[i] = mem_read1[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem1.mem_read[i][j] = mem_read1[i][j];
 
 $readmemh("memory_2.txt",mem_read2);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem2.mem_read[i] = mem_read2[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem2.mem_read[i][j] = mem_read2[i][j];
 
 $readmemh("memory_3.txt",mem_read3);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem3.mem_read[i] = mem_read3[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem3.mem_read[i][j] = mem_read3[i][j];
 
 $readmemh("memory_4.txt",mem_read4);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem4.mem_read[i] = mem_read4[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem4.mem_read[i][j] = mem_read4[i][j];
 
 $readmemh("memory_5.txt",mem_read5);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem5.mem_read[i] = mem_read5[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem5.mem_read[i][j] = mem_read5[i][j];
 
 $readmemh("memory_6.txt",mem_read6);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem6.mem_read[i] = mem_read6[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem6.mem_read[i][j] = mem_read6[i][j];
 
 $readmemh("memory_7.txt",mem_read7);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem7.mem_read[i] = mem_read7[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem7.mem_read[i][j] = mem_read7[i][j];
 
 $readmemh("memory_8.txt",mem_read8);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem8.mem_read[i] = mem_read8[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem8.mem_read[i][j] = mem_read8[i][j];
 
 $readmemh("memory_9.txt",mem_read9);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem9.mem_read[i] = mem_read9[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem9.mem_read[i][j] = mem_read9[i][j];
 
 $readmemh("memory_10.txt",mem_read10);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem10.mem_read[i] = mem_read10[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem10.mem_read[i][j] = mem_read10[i][j];
 
 $readmemh("memory_11.txt",mem_read11);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem11.mem_read[i] = mem_read11[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem11.mem_read[i][j] = mem_read11[i][j];
 
 $readmemh("memory_12.txt",mem_read12);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem12.mem_read[i] = mem_read12[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem12.mem_read[i][j] = mem_read12[i][j];
 
 $readmemh("memory_13.txt",mem_read13);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem13.mem_read[i] = mem_read13[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem13.mem_read[i][j] = mem_read13[i][j];
 
 $readmemh("memory_14.txt",mem_read14);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem14.mem_read[i] = mem_read14[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem14.mem_read[i][j] = mem_read14[i][j];
 
 $readmemh("memory_15.txt",mem_read15);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem15.mem_read[i] = mem_read15[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem15.mem_read[i][j] = mem_read15[i][j];
 
 $readmemh("memory_16.txt",mem_read16);
-for (i = 0; i < 4644; i = i + 1)
-pp.mem.mem16.mem_read[i] = mem_read16[i];
+for (i = 0; i < 66; i = i + 1)
+for (j = 0; j < 66; j = j + 1)
+pp.mem.mem16.mem_read[i][j] = mem_read16[i][j];
 
 clk = 0;
 forever #1 clk = ~clk;
